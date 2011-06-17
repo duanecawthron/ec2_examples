8d53b9fcf2b1dccbd64e6c128499a03b0b3f9fc2
Duane Cawthron <duane@studiodc.com>
2011-06-17 09:27:48 -0500

    add 002_capistrano_norails

---
+++ 002_capistrano_norails/005_deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source 000_config.sh
+
+# ---------------- https://github.com/capistrano/capistrano/wiki/2.x-From-The-Beginning
+
+git init
+git add .
+git commit -m'initial commit'
+
+cap deploy:setup
+cap deploy:check
+cap deploy:update
---
+++ 002_capistrano_norails/src/config/deploy.rb
@@ -0,0 +1,40 @@
+set :application, "norails"
+
+set :repository,  "."
+set :scm, :git
+# set :branch, "master"
+# set :git_shallow_clone, 1
+
+set :deploy_to, "/var/www/norails"
+set :deploy_via, :copy
+set :copy_exclude, [".git", ".DS_Store"]
+set :keep_releases, 2
+
+set :location, "ec2-204-236-222-167.compute-1.amazonaws.com"
+role :app, location
+role :web, location
+role :db,  location, :primary => true
+
+set :use_sudo, false
+set :user, "ec2-user"
+ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "server001keypair.pem")]
+
+#set :default_environment, {
+#  'PATH' => "/path/to/.rvm/gems/ree/1.8.7/bin:/path/to/.rvm/bin:/path/to/.rvm/ree-1.8.7-2009.10/bin:$PATH",
+#  'RUBY_VERSION' => 'ruby 1.8.7',
+#  'GEM_HOME'     => '/path/to/.rvm/gems/ree-1.8.7-2010.01',
+#  'GEM_PATH'     => '/path/to/.rvm/gems/ree-1.8.7-2010.01',
+#  'BUNDLE_PATH'  => '/path/to/.rvm/gems/ree-1.8.7-2010.01'
+#}
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
