90bffd4c4eb99718ab2e8e5547af543d086c88fe
Duane Cawthron <duane@studiodc.com>
2011-06-20 00:26:26 -0500

    call rvm use 1.8.7 before adding gems

--- 002_capistrano_norails/server/001_install_capistrano.sh
+++ 002_capistrano_norails/server/001_install_capistrano.sh
@@ -5,6 +5,7 @@
 source 000_config.sh
 source /etc/profile.d/rvm.sh
 
+rvm use 1.8.7
 rvm gemset use global
 rvm gemset create $PROJECT
 rvm use 1.8.7@$PROJECT
@@ -14,5 +15,7 @@ gem install capistrano --no-rdoc --no-ri
 sudo mkdir -p /u
 sudo chown ec2-user /u
 
-[ -f /etc/sudoers.orig ] || sudo cp -p /etc/sudoers /etc/sudoers.orig
-sudo vim -s comment_out_requiretty.vim /etc/sudoers
+if [ ! -f /etc/sudoers.orig ]; then
+	sudo cp -p /etc/sudoers /etc/sudoers.orig
+	sudo vim -s comment_out_requiretty.vim /etc/sudoers
+fi
