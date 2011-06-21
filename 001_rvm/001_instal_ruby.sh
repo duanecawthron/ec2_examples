9f9f1d2628c8ebe9bc5a3b7a19dce9ee367ea9bf
Duane Cawthron <duane@studiodc.com>
2011-06-21 07:05:08 -0500

    install ruby with openssl

--- 001_rvm/001_instal_ruby.sh
+++ 001_rvm/001_instal_ruby.sh
@@ -7,6 +7,15 @@ source /etc/profile.d/rvm.sh
 sudo yum -y install gcc.i686
 sudo yum -y install make.i686
 
+# rvm package -h
+#   Usage:
+#   'rvm package {install,uninstall} {readline,iconv,curl,openssl,zlib,autoconf,ncurses,pkgconfig,gettext,glib,mono,llvm,libxml2,libxslt,libyaml}'
+#   'ree_dependencies' installs zlib, ncurses, readline, openssl and iconv in this order.
+#   still need to add ' --with-readline-dir=$rvm_usr_path --with-iconv-dir=$rvm_usr_path --with-zlib-dir=$rvm_usr_path --with-openssl-dir=$rvm_usr_path' to 'rvm install ree'
+#
+# NOTE: Below uses $rvm_path/usr. Above uses $rvm_usr_path. It is not defined.
+
 rvm package install zlib
+rvm package install openssl		# required by thin
 rvm remove 1.8.7
-rvm install 1.8.7 --with-zlib-dir=$rvm_path/usr
+rvm install 1.8.7 --with-zlib-dir=$rvm_path/usr --with-openssl-dir=$rvm_path/usr
