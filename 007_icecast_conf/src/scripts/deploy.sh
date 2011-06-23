eee61c47f6ad93b12022fd62e5bf4424aaebe1b4
Duane Cawthron <duane@studiodc.com>
2011-06-23 08:42:03 -0500

    add comments

--- 007_icecast_conf/src/scripts/deploy.sh
+++ 007_icecast_conf/src/scripts/deploy.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+# ---------------- install configuration files for Icecast and MPD
+
 cap setup_icecast
+
+# ---------------- start the server
+
 cap restart_icecast
 cap restart_mpd
 cap play_test_song
+
+# ---------------- Use VLC to test the stream
+
+echo Use VLC to test the stream
+echo http://ec2-184-73-84-106.compute-1.amazonaws.com:8000/stream.mp3
+echo http://ec2-184-73-84-106.compute-1.amazonaws.com:8080/stream.ogg
