374bc57ed689c71696b9a53739875e81f101c9e1
Duane Cawthron <duane@studiodc.com>
2011-06-24 08:38:02 -0500

    remove debug statements

--- 003_nginx/005_deploy.sh
+++ 003_nginx/005_deploy.sh
@@ -7,12 +7,4 @@ cd $TOP
 
 rm -rf tmp
 cp -pr src tmp
-pwd
-ls ../002_capistrano_norails
-ls -l ../002_capistrano_norails/101_cap_deploy.sh
-
-
-
-rm -rf tmp
-cp -pr src tmp
 ../002_capistrano_norails/101_cap_deploy.sh tmp
